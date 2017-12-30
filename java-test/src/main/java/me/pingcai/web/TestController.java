164044e797520d277c603d4c761339feca857765
黄平财 <huangpingcai@meituan.com>
2017-12-30 18:17:11 +0800

    update

--- java-test/src/main/java/me/pingcai/web/TestController.java
+++ java-test/src/main/java/me/pingcai/web/TestController.java
@@ -1,10 +1,15 @@
 package me.pingcai.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * create by 黄平财 at 2017/11/30 22:52
@@ -12,12 +17,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class TestController {
-    @RequestMapping(value = "alive", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
-    public String alive() {
 
-
+    @RequestMapping(value = "alive",
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.HEAD, RequestMethod.PUT, RequestMethod.DELETE},
+            produces = MediaType.TEXT_HTML_VALUE)
+    public String alive(@RequestParam Map<String, Object> data) {
         log.info("Request success !");
-
+        if (MapUtils.isNotEmpty(data)) {
+            StringBuilder sb = new StringBuilder();
+            data.forEach((key, val) -> {
+                sb.append(key).append("=").append(val).append("&");
+            });
+            log.info("Request params : {}", sb.subSequence(0, sb.length() - 1));
+        }
         return "yes";
     }
+
+
+    @RequestMapping(value = "json",
+            method = {RequestMethod.POST, RequestMethod.HEAD, RequestMethod.PUT, RequestMethod.DELETE},
+            produces = MediaType.TEXT_HTML_VALUE)
+    public String json(@RequestBody Map<String, Object> data) {
+        log.info("Request success !");
+        if (MapUtils.isNotEmpty(data)) {
+            StringBuilder sb = new StringBuilder();
+            data.forEach((key, val) -> {
+                sb.append(key).append("=").append(val).append("&");
+            });
+            log.info("Request params : {}", sb.subSequence(0, sb.length() - 1));
+        }
+        return "yes";
+    }
+
 }
