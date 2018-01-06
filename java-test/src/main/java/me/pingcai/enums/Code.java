f777eed1e8392597118c3da96ffa67d1202dd70e
黄平财 <huangpingcai@meituan.com>
2018-01-07 00:39:21 +0800

    数据库组件

---
+++ java-test/src/main/java/me/pingcai/enums/Code.java
@@ -0,0 +1,11 @@
+package me.pingcai.enums;
+
+/**
+ * create by 黄平财 at 2018/1/6 00:54
+ */
+public interface Code {
+
+    String getCode();
+
+    String getData();
+}
---
+++ java-test/src/main/java/me/pingcai/vo/ResponseFactory.java
@@ -0,0 +1,17 @@
+package me.pingcai.vo;
+
+/**
+ * create by 黄平财 at 2018/1/6 00:49
+ */
+public final class ResponseFactory {
+
+    public static <T> Object buildSuccess(T t){
+        return null;
+    }
+
+    public static <T> Object buildError(T t){
+        return null;
+    }
+
+
+}
