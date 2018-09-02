7963830f41d55cfb95833796b0d62b50393dd211
黄平财 <huangpingcai@meituan.com>
2018-09-02 21:56:15 +0800

    test

---
+++ java-test/src/test/java/me/pingcai/ApplicationContextTests.java
@@ -0,0 +1,15 @@
+package me.pingcai;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author huangpingcai
+ * @since 2018/9/2 20:10
+ */
+@ContextConfiguration(value = "classpath:spring/spring-core.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationContextTests {
+
+}
